271e78503e4f88e50a8a0dd7598ea43972042b01
LeoKu <leokudev@gmail.com>
2025-05-12 23:36:22 +0800

    chore: 新增 Prettier 配置文件并更新依赖
    本次提交新增了 `.prettierrc.mjs` 配置文件，以统一代码格式化规则。同时，在 `package.json` 中添加了 Prettier 依赖，并更新了相关的脚本命令，以支持 MDX 文件的格式化。这些更改旨在提升代码的一致性和可读性。

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,8 @@ import type { NextConfig } from 'next'
 import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
 }
 
-const withMDX = createMDX({
-  extension: /\.(md|mdx)$/,
-})
+const withMDX = createMDX()
 
 export default withMDX(nextConfig)
