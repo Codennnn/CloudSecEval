8a006d27a7c7e79abdddc59ff413ac57c5964258
LeoKu <leokudev@gmail.com>
2025-05-12 23:02:13 +0800

    feat: 优化布局组件和新增文档布局组件
    本次提交对 `layout.tsx` 进行了优化，移除了不必要的样式类，提升了代码的简洁性。同时，新增了 `docs/layout.tsx` 文件，创建了文档布局组件，包含侧边栏、面包屑导航和内容区域，旨在为文档提供更好的结构和可读性。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="zh-CN">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="antialiased"
       >
         <ThemeProvider
           disableTransitionOnChange
