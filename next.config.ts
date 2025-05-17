1ddc1ec8a76d6085f6caedea7da6d7521d200da7
LeoKu <leokudev@gmail.com>
2025-05-17 09:45:15 +0800

    feat: 更新 Next.js 配置以支持 MDX 和添加 remark-gfm 插件
    本次提交更新了 `next.config.ts` 文件，配置了 MDX 扩展以支持 `.mdx` 文件，并添加了 `remark-gfm` 插件以增强 Markdown 语法支持。同时，更新了 `package.json` 和 `pnpm-lock.yaml` 文件，新增了 `remark-gfm` 依赖。这些更改旨在提升项目的文档处理能力和用户体验。

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from 'next'
 import createMDX from '@next/mdx'
+// import remarkGfm from 'remark-gfm'
 
 const nextConfig: NextConfig = {
 }
 
-const withMDX = createMDX()
+const withMDX = createMDX({
+  extension: /\.mdx$/,
+  options: {
+    remarkPlugins: [
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      ['remark-gfm', { strict: true, throwOnError: true }],
+    ],
+    rehypePlugins: [],
+  },
+})
 
 export default withMDX(nextConfig)
